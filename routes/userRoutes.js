17ac286c37e43c65cec81ee6cb2a53e46ac6b042
sandybhatta <sandipresponse256@gmail.com>
2025-07-01 22:42:22 +0530

    /update-profile api built for modify only some white listedfields , other sensitive fields are not updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,10 @@
 
 
 import express from "express"
-import{
-    deleteUser
-} from "../user.controllers/deleteUser.js"
+import deleteUser from "../user.controllers/deleteUser.js"
 
 import { protect } from "../middlewares/authMiddleware.js"
+import updateProfile from "../user.controllers/updateProfile.js"
 
 
 
@@ -23,5 +22,9 @@ import { protect } from "../middlewares/authMiddleware.js"
 const router=express.Router()
 
 
-
+// to delete the user and the documents from other models too
 router.delete("/delete",protect, deleteUser)
+
+// to update the profile
+
+router.patch("/update-profile",protect,updateProfile)
--- user.controllers/deleteUser.js
+++ user.controllers/deleteUser.js
@@ -7,7 +7,7 @@ import Token from "../models/token.js"; // Assuming you have a token model
 
 import { deleteCommentThread } from "../utils/deleteCommentThread.js";
 
-export const deleteUser = async (req, res) => {
+export default deleteUser = async (req, res) => {
   const { user } = req;
   const { password } = req.body;
 
